ed4f1466defcca407ff20f2c08c013f53c2dd633
Vazanoir <romain.gianolio@proton.me>
2023-11-09 15:19:45 +0100

    Update fedora-tweaks.sh

--- fedora-tweaks.sh
+++ fedora-tweaks.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-echo -e "Mets Flathub par défaut"
+# Enlève les flatpaks de Fedora, installe flathub, mets flatpak en premier dans Logiciels
 flatpak remote-delete fedora
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 gsettings set org.gnome.software packaging-format-preference "['flatpak', 'rpm']"
 
-echo -e "Mets le nombre de téléchargements en parallèle de DNF à 10"
+# Mets le nombre de téléchargements parallèles de DNF à 10
 echo -e "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 
-echo -e "Installation de Firefox Gnome Theme"
+# Installation de Firefox Gnome Theme
 curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash
 
-echo -e "Installation de MoreWaita"
+# Installation de MoreWaita
 dnf copr enable dusansimic/themes
 dnf install morewaita-icon-theme
 gsettings set org.gnome.desktop.interface icon-theme 'MoreWaita'
