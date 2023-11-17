c26892741891c20365f3880f4d6499e37d114b8a
Vazanoir <romain.gianolio@proton.me>
2023-11-17 19:00:46 +0100

    Update fedora-tweaks.sh

--- fedora-tweaks.sh
+++ fedora-tweaks.sh
@@ -1,24 +1,44 @@
 #!/usr/bin/env bash
 
-# Enlève les flatpaks de Fedora, installe flathub, mets flatpak en premier dans Logiciels
-flatpak remote-delete fedora
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-gsettings set org.gnome.software packaging-format-preference "['flatpak', 'rpm']"
-
-# Mets le nombre de téléchargements parallèles de DNF à 10
-echo -e "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+### APPARENCES -----------------------------------------------------------------
 
 # Installation de Firefox Gnome Theme
 curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash
 
 # Installation de MoreWaita
-sudo dnf copr enable dusansimic/themes
-sudo dnf install morewaita-icon-theme
+sudo dnf copr enable dusansimic/themes -y
+sudo dnf install morewaita-icon-theme -y
 gsettings set org.gnome.desktop.interface icon-theme 'MoreWaita'
+
+
+
+### AJUSTEMENTS ----------------------------------------------------------------
+
+# Mets le nombre de téléchargements parallèles de DNF à 10
+CONTENT="max_parallel_downloads=10"
+FILE="/etc/dnf/dnf.conf"
+if ! grep -q "$CONTENT" "$FILE"; then
+	echo -e $CONTENT | sudo tee -a $FILE > /dev/null
+fi
+
+# Enlève les flatpaks de Fedora, installe flathub, mets flatpak en premier dans Logiciels
+if flatpak remotes | grep -q fedora; then
+	flatpak remote-delete fedora
+fi
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+gsettings set org.gnome.software packaging-format-preference "['flatpak', 'rpm']"
+
+
+
+### CORRECTIONS DE BUGS --------------------------------------------------------
 
 # Règle le problème entre SELinux et les jeux Source
 sudo setsebool -P allow_execheap 1
 
 # Règle un problème pour certains gros jeux
-echo -e "vm.max_map_count = 16777216" | sudo tee -a /etc/sysctl.conf > /dev/null
-sudo sysctl -p
+CONTENT="vm.max_map_count = 16777216"
+FILE="/etc/sysctl.conf"
+if ! grep -q "$CONTENT" "$FILE"; then
+	echo -e $CONTENT | sudo tee -a $FILE > /dev/null
+	sudo sysctl -p
+fi
