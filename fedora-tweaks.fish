70d8aabcb696f9ef5040e898ab5874b97a549a6d
Vazanoir <8283348-vazanoir@users.noreply.gitlab.com>
2024-09-05 18:01:41 +0200

    Add english comments

--- fedora-tweaks.fish
+++ fedora-tweaks.fish
@@ -1,12 +1,12 @@
 #!/usr/bin/env fish
 
-### APPARENCES -----------------------------------------------------------------
+### LOOKS -----------------------------------------------------------------
 
-# Installation de Betterfox et de Firefox Gnome Theme
+# Install Betterfox and Firefox Gnome Theme
 curl -s -o- https://raw.githubusercontent.com/vazanoir/update-betterfox/main/install-by-curl.sh | bash
 curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash
 
-# Installation de MoreWaita
+# Install MoreWaita
 if not dnf list installed | grep -q morewaita
 	sudo dnf copr enable dusansimic/themes -y
 	sudo dnf install morewaita-icon-theme -y
@@ -15,16 +15,15 @@ end
 
 
 
-### AJUSTEMENTS ----------------------------------------------------------------
+### TWEAKS ----------------------------------------------------------------
 
-# Mets le nombre de téléchargements parallèles de DNF à 10
 set content "max_parallel_downloads=10"
 set file "/etc/dnf/dnf.conf"
 if not grep -q $content $file
 	echo -e $content | sudo tee -a $file > /dev/null
 end
 
-# Enlève les flatpaks de Fedora, installe flathub, mets flatpak en premier dans Logiciels
+# Remove Fedora's flatpaks, install flathub for system, user and beta branch
 if flatpak remotes | grep -q fedora
 	flatpak remote-delete fedora
 end
@@ -32,18 +31,20 @@ end
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+
+# Set flatpak first in Gnome's Software
 gsettings set org.gnome.software packaging-format-preference "['flatpak:flathub', 'flatpak:flathub-beta', 'rpm']"
 
 
 
-### CORRECTIONS DE BUGS --------------------------------------------------------
+### BUGFIXES --------------------------------------------------------
 
-# Règle le problème entre SELinux et les jeux Source
+# Fix issue between SELinux and Source games
 if getsebool allow_execheap | grep -q off
 	sudo setsebool -P allow_execheap 1
 end
 
-# Règle un problème pour certains gros jeux
+# Fix issue with big games
 set content "vm.max_map_count = 16777216"
 set file "/etc/sysctl.conf"
 if not grep -q $content $file
