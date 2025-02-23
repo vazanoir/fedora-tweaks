e7daec280652e78b2159763fa0e2ef074163fce2
Vazanoir <romain.gianolio@proton.me>
2025-02-23 19:43:20 +0100

    add flatpak remotes tweaks

--- tweaks.go
+++ tweaks.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"io"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -44,10 +45,39 @@ func tweaks() []tweak {
 			selectedByDefault: true,
 		},
 		tweak{
-			name:              "Remove Fedora Flatpak",
-			desc:              "Remove the Fedora Flatpak apps and repository.",
-			callback:          func() error { return nil },
-			selectedByDefault: false,
+			name: "Remove Fedora flatpak remote",
+			desc: "Remove the Fedora Flatpak apps and repository.",
+			callback: func() error {
+				stdOut, err := exec.Command("flatpak", "remotes").Output()
+				if err != nil {
+					return err
+				}
+
+				if !strings.Contains(string(stdOut), "fedora") {
+					return nil
+				}
+
+				_, err = exec.Command("flatpak", "remote-delete", "fedora").Output()
+				if err != nil {
+					return err
+				}
+
+				return nil
+			},
+			selectedByDefault: true,
+		},
+		tweak{
+			name: "Add Flathub flatpak remote",
+			desc: "Add the most popular remote for flatpaks.",
+			callback: func() error {
+				_, err := exec.Command("flatpak", "remote-add", "--if-not-exists", "flathub", "https://dl.flathub.org/repo/flathub.flatpakrepo").Output()
+				if err != nil {
+					return err
+				}
+
+				return nil
+			},
+			selectedByDefault: true,
 		},
 		tweak{
 			name:              "Set flatpak as default in Gnome Software",
@@ -59,31 +89,31 @@ func tweaks() []tweak {
 			name:              "Load i2c-dev and i2c-piix4 kernel modules",
 			desc:              "Load needed kernel modules for hardware detection in software like OpenRGB.",
 			callback:          func() error { return nil },
-			selectedByDefault: true,
+			selectedByDefault: false,
 		},
 		tweak{
 			name:              "Install systemd-container",
 			desc:              "Install the systemd-container dnf package, mainly with GDM Settings in mind.",
 			callback:          func() error { return nil },
-			selectedByDefault: true,
+			selectedByDefault: false,
 		},
 		tweak{
 			name:              "Fix issue between SELinux and Source games",
 			desc:              "Install the systemd-container dnf package, mainly with GDM Settings in mind.",
 			callback:          func() error { return nil },
-			selectedByDefault: true,
+			selectedByDefault: false,
 		},
 		tweak{
 			name:              "Fix issue with big games",
 			desc:              "Install the systemd-container dnf package, mainly with GDM Settings in mind.",
 			callback:          func() error { return nil },
-			selectedByDefault: true,
+			selectedByDefault: false,
 		},
 		tweak{
 			name:              "Install non-free p7zip with unrar capacities",
 			desc:              "Install the systemd-container dnf package, mainly with GDM Settings in mind.",
 			callback:          func() error { return nil },
-			selectedByDefault: true,
+			selectedByDefault: false,
 		},
 	}
 }
